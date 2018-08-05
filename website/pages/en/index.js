ee75a9d33a25f2a0d490e7ba2a8afb1c371cc2b3
siderakis <nixsid@gmail.com>
2018-08-05 17:24:25 -0400

    resizing homepage image

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -12,16 +12,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-const React = require('react');
-const siteConfig = require(process.cwd() + '/siteConfig.js');
+const React = require("react")
+const siteConfig = require(process.cwd() + "/siteConfig.js")
 
 const Button = props => (
   <div className="pluginWrapper buttonWrapper">
-    <a className="button" href={props.href} target={props.target || '_self'}>
+    <a className="button" href={props.href} target={props.target || "_self"}>
       {props.children}
     </a>
   </div>
-);
+)
 
 const SplashContainer = props => (
   <div className="homeContainer">
@@ -29,7 +29,7 @@ const SplashContainer = props => (
       <div className="wrapper homeWrapper">{props.children}</div>
     </div>
   </div>
-);
+)
 
 const PromoSection = props => (
   <div className="section promoSection">
@@ -37,21 +37,21 @@ const PromoSection = props => (
       <div className="pluginRowBlock">{props.children}</div>
     </div>
   </div>
-);
+)
 
-const imgUrl = img => siteConfig.baseUrl + img;
+const imgUrl = img => siteConfig.baseUrl + img
 
 class Index extends React.Component {
   docUrl(doc) {
-    const language = this.props.language || '';
-    return siteConfig.baseUrl + 'docs/' + (language ? language + '/' : '') + doc;
+    const language = this.props.language || ""
+    return siteConfig.baseUrl + "docs/" + (language ? language + "/" : "") + doc
   }
   render() {
     return (
       <div>
         <SplashContainer>
           <div className="projectLogo">
-            <img src={imgUrl('rejoiner_logo.svg')} />
+            <img src={imgUrl("rejoiner_logo.svg")} />
           </div>
           <div className="inner">
             <h2 className="projectTitle">
@@ -65,11 +65,14 @@ class Index extends React.Component {
           </div>
         </SplashContainer>
         <div className="mainContainer alignCenter">
-          <img src={imgUrl('rejoiner_overview.svg')} />
+          <img
+            src={imgUrl("rejoiner_overview.svg")}
+            style={{ maxWidth: 750, width: "100%" }}
+          />
         </div>
       </div>
-    );
+    )
   }
 }
 
-module.exports = Index;
+module.exports = Index
