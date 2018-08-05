cf84b18a34faf6882774399e4c25b7ede2402c70
siderakis <nixsid@gmail.com>
2018-08-05 17:16:57 -0400

    Converting ExecutionResults into ProtoExecutionResults

---
+++ rejoiner/src/main/java/com/google/api/graphql/execution/ExecutionResultToProtoAsync.java
@@ -0,0 +1,32 @@
+package com.google.api.graphql.execution;
+
+import com.google.api.graphql.grpc.QueryResponseToProto;
+import com.google.protobuf.Message;
+import graphql.ExecutionResult;
+
+import java.util.concurrent.CompletableFuture;
+
+/** Transforms ExecutionResult into generated proto Messages. */
+public final class ExecutionResultToProtoAsync {
+
+  /** Transforms an async ExecutionResult into a ProtoExecutionResult. */
+  public static <T extends Message>
+      CompletableFuture<ProtoExecutionResult<T>> toProtoExecutionResult(
+          T message, CompletableFuture<ExecutionResult> executionResultCompletableFuture) {
+    return executionResultCompletableFuture.thenApply(
+        executionResult ->
+            ProtoExecutionResult.create(
+                QueryResponseToProto.buildMessage(message, executionResult.toSpecification())));
+  }
+  /**
+   * Transforms an async ExecutionResult into a proto Messages.
+   *
+   * <p>Note that only data and not error information is represented in the Proto.
+   */
+  public static <T extends Message> CompletableFuture<T> toProtoMessage(
+      T message, CompletableFuture<ExecutionResult> executionResultCompletableFuture) {
+    return executionResultCompletableFuture.thenApply(
+        executionResult ->
+            QueryResponseToProto.buildMessage(message, executionResult.toSpecification()));
+  }
+}
