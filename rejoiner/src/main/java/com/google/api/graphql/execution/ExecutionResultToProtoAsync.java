988cd239fc6a1264bc6bdb881f197840fc32a716
Nick Siderakis <nixsid@gmail.com>
2018-08-05 20:02:23 -0400

    mapping errors to static proto (wip)

--- rejoiner/src/main/java/com/google/api/graphql/execution/ExecutionResultToProtoAsync.java
+++ rejoiner/src/main/java/com/google/api/graphql/execution/ExecutionResultToProtoAsync.java
@@ -1,8 +1,25 @@
+// Copyright 2017 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.google.api.graphql.execution;
 
 import com.google.api.graphql.grpc.QueryResponseToProto;
 import com.google.protobuf.Message;
 import graphql.ExecutionResult;
+import graphql.ErrorType;
+import graphql.ExecutionResult;
+import graphql.GraphQLError;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -29,4 +46,43 @@ public final class ExecutionResultToProtoAsync {
         executionResult ->
             QueryResponseToProto.buildMessage(message, executionResult.toSpecification()));
   }
+  
+  
+   private static final ImmutableMap<
+            ErrorType, com.google.api.graphql.ErrorType>
+        ERROR_TYPE_MAP =
+            ImmutableMap.of(
+                ErrorType.DataFetchingException,
+                com.google.api.graphql.ErrorType.DATA_FETCHING_EXCEPTION,
+                ErrorType.InvalidSyntax,
+                com.google.api.graphql.ErrorType.INVALID_SYNTAX,
+                ErrorType.ValidationError,
+                com.google.api.graphql.ErrorType.VALIDATION_ERROR);
+
+    private static ImmutableList<GraphQlError> errorsToProto(List<GraphQLError> errors) {
+      return errors
+          .stream()
+          .map(
+              error ->
+                  GraphQlError.newBuilder()
+                      .setMessage(error.getMessage())
+                      .setType(
+                          ERROR_TYPE_MAP.getOrDefault(
+                              error.getErrorType(),
+                              com.google.api.graphql.ErrorType.UNKNOWN))
+                      .addAllLocations(
+                          error
+                              .getLocations()
+                              .stream()
+                              .map(
+                                  location ->
+                                      SourceLocation.newBuilder()
+                                          .setLine(location.getLine())
+                                          .setColumn(location.getColumn())
+                                          .build())
+                              .collect(ImmutableList.toImmutableList()))
+                      .build())
+          .collect(ImmutableList.toImmutableList());
+    }
+
 }
