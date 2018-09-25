google/rejoiner

commit 6a9323c0c5a9c84e59fcf95f5df16c3e74c2469a

Convert field name to camel case for GraphqlInputObjecType

rejoiner/src/main/java/com/google/api/graphql/rejoiner/GqlInputConverter.java
@@ -16,6 +16,8 @@ package com.google.api.graphql.rejoiner;
 
 import static graphql.Scalars.GraphQLString;
 
+import com.google.common.base.CaseFormat;
+import com.google.common.base.Converter;
 import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
 import com.google.common.collect.ImmutableBiMap;
@@ -47,6 +49,9 @@ final class GqlInputConverter {
 
   private final BiMap<String, Descriptor> descriptorMapping;
   private final BiMap<String, EnumDescriptor> enumMapping;
+
+  private static final Converter<String, String> UNDERSCORE_TO_CAMEL =
+          CaseFormat.LOWER_UNDERSCORE.converterTo(CaseFormat.LOWER_CAMEL);
 
   private GqlInputConverter(
       BiMap<String, Descriptor> descriptorMapping, BiMap<String, EnumDescriptor> enumMapping) {
@@ -94,8 +99,10 @@ final class GqlInputConverter {
     }
     for (FieldDescriptor field : descriptor.getFields()) {
       GraphQLType fieldType = getFieldType(field);
+      String fieldName = field.getName();
+      String convertedFieldName = fieldName.contains("_") ? UNDERSCORE_TO_CAMEL.convert(fieldName) : fieldName;
       GraphQLInputObjectField.Builder inputBuilder =
-          GraphQLInputObjectField.newInputObjectField().name(field.getName());
+          GraphQLInputObjectField.newInputObjectField().name(convertedFieldName);
       if (field.isRepeated()) {
         inputBuilder.type(new GraphQLList(fieldType));
       } else {

rejoiner/src/test/java/com/google/api/graphql/rejoiner/GqlInputConverterTest.java
@@ -21,6 +21,9 @@ import com.google.common.truth.Truth;
 import com.google.common.truth.extensions.proto.ProtoTruth;
 import com.google.protobuf.Message;
 import graphql.schema.GraphQLArgument;
+import graphql.schema.GraphQLInputObjectType;
+import graphql.schema.GraphQLInputType;
+import graphql.schema.GraphQLType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -63,6 +66,16 @@ public final class GqlInputConverterTest {
     Truth.assertThat(argument.getType().getName())
         .isEqualTo("Input_javatests_com_google_api_graphql_rejoiner_proto_Proto1");
   }
+
+  @Test
+  public void inputConverterShouldCreateInputTypeWithCamelCaseName() {
+    GqlInputConverter inputConverter =
+            GqlInputConverter.newBuilder().add(TestProto.getDescriptor().getFile()).build();
+    GraphQLInputObjectType input = (GraphQLInputObjectType) inputConverter.getInputType(Proto1.getDescriptor());
+    Truth.assertThat(input.getField("intField")).isNotNull();
+    Truth.assertThat(input.getField("camelCaseName")).isNotNull();
+  }
+
 
   @Test
   public void inputConverterShouldCreateArgumentForMessagesInSameFile() {

commit 47fdced6c51a395d16d6f8587814c78b8a92fdd7

Fix GqlInputConverter#createProtoBuf() bug.
Bug was introduced in 6a9323c0c5a9c84e59fcf95f5df16c3e74c2469a. This
commit aimed to convert underscore_case field names to camelCase.
While the GraphQLInputObjectField's name was converted,
GqlInputConverter#createProtoBuf(), which converts Map<String, Object>
to a protobuf Message, did not do the same conversion. This resulted
in fields like "id" working, but fields like "user_id" being ignored.

This change fixes the bug and adds an assertion that all input fields
were consumed.

rejoiner/src/main/java/com/google/api/graphql/rejoiner/GqlInputConverter.java
@@ -15,7 +15,6 @@
 package com.google.api.graphql.rejoiner;
 
 import static graphql.Scalars.GraphQLString;
-
 import com.google.common.base.CaseFormat;
 import com.google.common.base.Converter;
 import com.google.common.collect.BiMap;
@@ -35,6 +34,7 @@ import graphql.schema.GraphQLList;
 import graphql.schema.GraphQLType;
 import graphql.schema.GraphQLTypeReference;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -51,7 +51,7 @@ final class GqlInputConverter {
   private final BiMap<String, EnumDescriptor> enumMapping;
 
   private static final Converter<String, String> UNDERSCORE_TO_CAMEL =
-          CaseFormat.LOWER_UNDERSCORE.converterTo(CaseFormat.LOWER_CAMEL);
+      CaseFormat.LOWER_UNDERSCORE.converterTo(CaseFormat.LOWER_CAMEL);
 
   private GqlInputConverter(
       BiMap<String, Descriptor> descriptorMapping, BiMap<String, EnumDescriptor> enumMapping) {
@@ -69,22 +69,29 @@ final class GqlInputConverter {
     if (input == null) {
       return builder.build();
     }
-    for (FieldDescriptor field : descriptor.getFields()) {
-      String fieldName = field.getName();
 
-      if (!input.containsKey(fieldName)) {
+    Map<String, Object> remainingInput = new HashMap<>(input);
+    for (FieldDescriptor field : descriptor.getFields()) {
+      String fieldName = getFieldName(field);
+
+      if (!remainingInput.containsKey(fieldName)) {
         // TODO: validate required fields
         continue;
       }
 
       if (field.isRepeated()) {
-        List<Object> values = (List<Object>) input.get(fieldName);
+        List<Object> values = (List<Object>) remainingInput.remove(fieldName);
         for (Object value : values) {
           builder.addRepeatedField(field, getValueForField(field, value, builder));
         }
       } else {
-        builder.setField(field, getValueForField(field, input.get(fieldName), builder));
+        builder.setField(field, getValueForField(field, remainingInput.remove(fieldName), builder));
       }
+    }
+
+    if (!remainingInput.isEmpty()) {
+      throw new AssertionError(
+          "All fields in input should have been consumed. Remaining: " + remainingInput);
     }
 
     return builder.build();
@@ -99,10 +106,8 @@ final class GqlInputConverter {
     }
     for (FieldDescriptor field : descriptor.getFields()) {
       GraphQLType fieldType = getFieldType(field);
-      String fieldName = field.getName();
-      String convertedFieldName = fieldName.contains("_") ? UNDERSCORE_TO_CAMEL.convert(fieldName) : fieldName;
       GraphQLInputObjectField.Builder inputBuilder =
-          GraphQLInputObjectField.newInputObjectField().name(convertedFieldName);
+          GraphQLInputObjectField.newInputObjectField().name(getFieldName(field));
       if (field.isRepeated()) {
         inputBuilder.type(new GraphQLList(fieldType));
       } else {
@@ -138,6 +143,12 @@ final class GqlInputConverter {
 
   static String getReferenceName(GenericDescriptor descriptor) {
     return "Input_" + ProtoToGql.getReferenceName(descriptor);
+  }
+
+  /** Field names with under_scores are converted to camelCase. */
+  private String getFieldName(FieldDescriptor field) {
+    String fieldName = field.getName();
+    return fieldName.contains("_") ? UNDERSCORE_TO_CAMEL.convert(fieldName) : fieldName;
   }
 
   private GraphQLType getFieldType(FieldDescriptor field) {

rejoiner/src/test/java/com/google/api/graphql/rejoiner/GqlInputConverterTest.java
@@ -22,8 +22,6 @@ import com.google.common.truth.extensions.proto.ProtoTruth;
 import com.google.protobuf.Message;
 import graphql.schema.GraphQLArgument;
 import graphql.schema.GraphQLInputObjectType;
-import graphql.schema.GraphQLInputType;
-import graphql.schema.GraphQLType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -47,7 +45,7 @@ public final class GqlInputConverterTest {
             Proto1.getDescriptor(),
             Proto1.newBuilder(),
             ImmutableMap.of(
-                "id", "id", "int_field", 123, "test_proto", ImmutableMap.of("inner_id", "1")));
+                "id", "id", "intField", 123, "testProto", ImmutableMap.of("innerId", "1")));
     ProtoTruth.assertThat(protoBuf)
         .isEqualTo(
             Proto1.newBuilder()
@@ -70,12 +68,12 @@ public final class GqlInputConverterTest {
   @Test
   public void inputConverterShouldCreateInputTypeWithCamelCaseName() {
     GqlInputConverter inputConverter =
-            GqlInputConverter.newBuilder().add(TestProto.getDescriptor().getFile()).build();
-    GraphQLInputObjectType input = (GraphQLInputObjectType) inputConverter.getInputType(Proto1.getDescriptor());
+        GqlInputConverter.newBuilder().add(TestProto.getDescriptor().getFile()).build();
+    GraphQLInputObjectType input =
+        (GraphQLInputObjectType) inputConverter.getInputType(Proto1.getDescriptor());
     Truth.assertThat(input.getField("intField")).isNotNull();
     Truth.assertThat(input.getField("camelCaseName")).isNotNull();
   }
-
 
   @Test
   public void inputConverterShouldCreateArgumentForMessagesInSameFile() {