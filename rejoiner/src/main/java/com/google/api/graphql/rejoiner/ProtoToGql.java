2ecdaa2a0489cddbe95730228a962416bf3f4f3f
Denis Sborets <Denis.Sborets@zaplabs.com>
2018-09-28 10:45:47 -0700

    Using field's Json name instead of internal conversion from snake case to camel case

--- rejoiner/src/main/java/com/google/api/graphql/rejoiner/ProtoToGql.java
+++ rejoiner/src/main/java/com/google/api/graphql/rejoiner/ProtoToGql.java
@@ -77,8 +77,6 @@ final class ProtoToGql {
           .put(Type.SFIXED64, Scalars.GraphQLLong)
           .build();
 
-  private static final Converter<String, String> UNDERSCORE_TO_CAMEL =
-      CaseFormat.LOWER_UNDERSCORE.converterTo(CaseFormat.LOWER_CAMEL);
   private static final Converter<String, String> LOWER_CAMEL_TO_UPPER =
       CaseFormat.LOWER_CAMEL.converterTo(CaseFormat.UPPER_CAMEL);
   private static final FieldConverter FIELD_CONVERTER = new FieldConverter();
@@ -147,14 +145,13 @@ final class ProtoToGql {
 
     @Override
     public GraphQLFieldDefinition apply(FieldDescriptor fieldDescriptor) {
-      String fieldName = fieldDescriptor.getName();
-      String convertedFieldName = fieldName.contains("_") ? UNDERSCORE_TO_CAMEL.convert(fieldName) : fieldName;
+      String fieldName = fieldDescriptor.getJsonName();
       GraphQLFieldDefinition.Builder builder =
           GraphQLFieldDefinition.newFieldDefinition()
               .type(convertType(fieldDescriptor))
               .dataFetcher(
-                  new ProtoDataFetcher(convertedFieldName))
-              .name(convertedFieldName);
+                  new ProtoDataFetcher(fieldName))
+              .name(fieldName);
       if (fieldDescriptor.getFile().toProto().getSourceCodeInfo().getLocationCount()
           > fieldDescriptor.getIndex()) {
         builder.description(
